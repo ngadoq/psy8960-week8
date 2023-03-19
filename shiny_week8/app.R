d59515de245ec8b1d86208bf0324544c92d75026
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-03-19 11:41:29 -0500

    Input

--- shiny_week8/app.R
+++ shiny_week8/app.R
@@ -1,12 +1,3 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
@@ -18,11 +9,9 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            selectInput('gender', "Select Gender", choices=c("Male", "Female", selected="All")),
+            selectInput('errorbar', "Display Error Bar?", choices=c("Display Error Band", "Suppress Error Band"), selected="Display Error Band"),
+            selectInput('completion_date', "Include participants who completed before August 1, 2017?", choices = c("Include", "Exclude"), selected = "Include"))
         ),
 
         # Show a plot of the generated distribution
