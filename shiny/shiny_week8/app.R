4b4b27d3e711681c0a367e5eda29b4834c4f90c4
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-03-19 18:44:48 -0500

    FInal

--- shiny/shiny_week8/app.R
+++ shiny/shiny_week8/app.R
@@ -1,50 +1,56 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidyverse)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+  
+  # Application title
+  titlePanel("Gender and Scores"),
+  
+  # Sidebar to separate from the main panel
+  sidebarLayout(
+    # All inputs are placed in the side panel to direct user attention
+    sidebarPanel(
+      # Add select input so users can filter based on gender
+      selectInput('gender', "Select Gender", choices=c("Male", "Female", "All"), selected="All"),
+      # Add select input so users can choose to display the error band in the plot
+      selectInput('error_band', "Display Error Band?", choices=list("Display Error Band", "Suppress Error Band"), selected="Display Error Band"),
+      # Add select input so users can choose whether to exclude participants who completed the assessment before August 1 2017
+      selectInput('timeEnd', "Include participants who completed before August 1, 2017?", choices = c("Include", "Exclude"), selected = "Include"))
+    ,
+    
+    # Show the plot in the main panel
+    mainPanel(plotOutput("scatterplot"))
+  )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+server <- function(input, output, session) {
+  # Read data 
+  shiny_tbl <- readRDS("shiny.rds")
+  # Create reactive expression to use later for plot
+  df <- reactive({
+    shiny_tbl %>% 
+      # Filter original data based on users' gender input and don't filter if user chooses "All"
+      filter(if(input$gender != "All") {gender == input$gender} else TRUE,
+             # Filter based on user's time completion input and don't filter if user chooses Include
+             if(input$timeEnd == "Exclude") {timeEnd >= 2017-08-01} else TRUE)    
     })
+  # Create scatter plot
+  output$scatterplot <- renderPlot({
+  # Recode user's error band input to since se from geom_smooth only takes logical vector
+  error_band <-  ifelse (input$error_band == "Display Error Band", TRUE, FALSE)
+  # Create scatterplot using ggplot to show relationship between two mean scores 
+    ggplot(df(), aes(x=mean_q1_q6, y=mean_q8_q10)) + 
+      # Use geom_jitter for better visualization
+      geom_jitter() + 
+      # Add regression line and error band based on user input
+      geom_smooth(method = "lm", color = "purple", se = error_band) +
+      # Add label for xasix, yxis and plot
+      labs(x="Mean q1-q6",
+           y="Mean q8-q10",
+           title = "Scatterplot of mean q1-q6 and mean q8-q10")
+  })
 }
 
 # Run the application 
