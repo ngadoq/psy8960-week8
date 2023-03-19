055a56377d7d43a63ef53554e93023ff44310844
ngaqdo <93609779+do000100@users.noreply.github.com>
2023-03-19 12:56:36 -0500

    saveRDS

--- shiny_week8/app.R
+++ shiny_week8/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+#saveRDS(file="../data/shiny.csv")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -10,29 +11,34 @@ ui <- fluidPage(
     sidebarLayout(
         sidebarPanel(
             selectInput('gender', "Select Gender", choices=c("Male", "Female", selected="All")),
-            selectInput('errorbar', "Display Error Bar?", choices=c("Display Error Band", "Suppress Error Band"), selected="Display Error Band"),
+            selectInput('error_bar', "Display Error Bar?", choices=c("Display Error Band", "Suppress Error Band"), selected="Display Error Band"),
             selectInput('completion_date', "Include participants who completed before August 1, 2017?", choices = c("Include", "Exclude"), selected = "Include"))
-        ),
+        ,
 
         # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
+        mainPanel(plotOutput("scatterplot"))
         )
-    )
 )
+
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  error_bar <- reactive(if (input$error_bar == "Display Error Band") TRUE else FALSE)
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
+  output$scatterplot <- renderPlot({
+        
         # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+        shiny_tbl %>% 
+          filter(
+            gender == input$gender,
+            timeEnd <= input$completion_date
+          ) %>% 
+          ggplot(aes(x=mean_q1_q6, y=mean_q8_q10)) + 
+          geom_jitter() + 
+          geom_smooth(method = "lm", color = "purple", se = error_bar ) +
+          labs(x="Mean q1-q6",
+               y="Mean q8-q10",
+               title = "Scatterplot of mean q1-q6 and mean q8-q10")
     })
 }
 
